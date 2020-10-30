f26ab1876e218a2192a959ac86cf6465be6a9baa
mohit8382 <mohitrecson1610@gmail.com>
2020-10-30 17:18:24 +0530

    added icon on website

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:my_webiste/progress_bar.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
   runApp(MaterialApp(home: MyApp(),));
@@ -8,11 +10,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return SafeArea(
       child: WebviewScaffold(
 
           appBar: AppBar(title: Text("Google"),),
-          url: "https://github.com/gautam2021/my_website"
+          url: "https://github.com/gautam2021/my_website",
+          initialChild: ProgressScreen(),
       ),
     );
   }
